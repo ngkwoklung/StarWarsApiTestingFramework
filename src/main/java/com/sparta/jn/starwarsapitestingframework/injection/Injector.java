5f5bbad6d7ad8f46a2bf19a345eecebd3f24cb44
Jimmy Ng <ngkwoklung@gmail.com>
2022-08-24 15:14:34 +0100

    Injection class added

---
+++ src/main/java/com/sparta/jn/starwarsapitestingframework/injection/Injector.java
@@ -0,0 +1,21 @@
+package com.sparta.jn.starwarsapitestingframework.injection;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.jn.starwarsapitestingframework.dto.PeopleDTO;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Injector {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    public static PeopleDTO injectActivityDTO(String path) {
+        PeopleDTO dto = new PeopleDTO();
+
+        try {
+            dto = mapper.readValue(new URL(path), PeopleDTO.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dto;
+    }
+}
