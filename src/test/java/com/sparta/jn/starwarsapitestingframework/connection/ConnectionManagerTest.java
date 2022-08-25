8535a0a590a0c9e12b7eac3a2d3e072d12098009
Keivi <kei_vi@hotmail.co.uk>
2022-08-25 11:33:42 +0100

    Tests for ConnectionManager.

--- src/test/java/com/sparta/jn/starwarsapitestingframework/connection/ConnectionManagerTest.java
+++ src/test/java/com/sparta/jn/starwarsapitestingframework/connection/ConnectionManagerTest.java
@@ -9,15 +9,14 @@ public class ConnectionManagerTest {
     @Test
     @DisplayName("Check url is correct when passing strings")
     void checkUrlIsCorrectWhenPassingStrings() {
-        Assertions.assertEquals(ConnectionManager.getConnectionURL("people","1"),
-                "https://swapi.dev/api/people/1");
+        Assertions.assertEquals("https://swapi.dev/api/people/1?format=json", ConnectionManager.getConnectionURL("people","1"));
     }
 
     @Test
     @DisplayName("Check url is correct when passing string and an int")
     void checkUrlIsCorrectWhenPassingStringAndAnInt() {
-        Assertions.assertEquals(ConnectionManager.getConnectionURL("people",1),
-                "https://swapi.dev/api/people/1");
+        System.out.println(ConnectionManager.getConnectionURL("people",1));
+        Assertions.assertEquals("https://swapi.dev/api/people/1?format=json", ConnectionManager.getConnectionURL("people",1));
     }
 
     @Test
@@ -29,7 +28,36 @@ public class ConnectionManagerTest {
     @Test
     @DisplayName("Check response body")
     void checkResponseBody() {
-
+        Assertions.assertEquals("{\"name\":\"Luke Skywalker\",\"height\":\"172\",\"mass\":\"77\",\"hair_color\":\"blond\",\"skin_color\":\"fair\",\"eye_color\":\"blue\",\"birth_year\":\"19BBY\",\"gender\":\"male\",\"homeworld\":\"https://swapi.dev/api/planets/1/\",\"films\":[\"https://swapi.dev/api/films/1/\",\"https://swapi.dev/api/films/2/\",\"https://swapi.dev/api/films/3/\",\"https://swapi.dev/api/films/6/\"],\"species\":[],\"vehicles\":[\"https://swapi.dev/api/vehicles/14/\",\"https://swapi.dev/api/vehicles/30/\"],\"starships\":[\"https://swapi.dev/api/starships/12/\",\"https://swapi.dev/api/starships/22/\"],\"created\":\"2014-12-09T13:50:51.644000Z\",\"edited\":\"2014-12-20T21:17:56.891000Z\",\"url\":\"https://swapi.dev/api/people/1/\"}", ConnectionManager.getResponseBody("people", 1));
     }
 
+    @Test
+    @DisplayName("Check server header is correct")
+    void checkServerHeaderIsCorrect(){
+        Assertions.assertEquals("nginx/1.16.1",ConnectionManager.getHeader("Server"));
+    }
+
+    @Test
+    @DisplayName("CheckThatContentTypeHeaderIs Application/json")
+    void checkThatContentTypeHeaderIsApplicationJson(){
+        Assertions.assertEquals("application/json", ConnectionManager.getHeader("Content-Type"));
+    }
+    
+    @Test
+    @DisplayName("Check that header Vary returns Accept cookie")
+    void checkThatHeaderVaryReturnsAcceptCookie(){
+        Assertions.assertEquals("Accept, Cookie", ConnectionManager.getHeader("Vary"));
+    }
+    
+    @Test
+    @DisplayName("Check that header X-Frame-Options returns correct value")
+    void checkThatHeaderXFrameOptionsReturnsCorrectValue(){
+        Assertions.assertEquals("SAMEORIGIN", ConnectionManager.getHeader("X-Frame-Options"));
+    }
+    
+    @Test
+    @DisplayName("Check that Allow header has correct value")
+    void checkThatAllowHeaderHasCorrectValue(){
+        Assertions.assertEquals("GET, HEAD, OPTIONS", ConnectionManager.getHeader("Allow"));
+    }
 }
