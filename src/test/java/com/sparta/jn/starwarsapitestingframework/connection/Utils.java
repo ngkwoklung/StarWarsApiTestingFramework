70de0e436cac688de2e089f0943ad3041cf51a26
Jimmy Ng <ngkwoklung@gmail.com>
2022-12-02 16:17:17 +0000

    Added Utils class

---
+++ src/test/java/com/sparta/jn/starwarsapitestingframework/connection/Utils.java
@@ -0,0 +1,46 @@
+package com.sparta.jn.starwarsapitestingframework.connection;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.*;
+import java.util.Properties;
+
+public class Utils {
+    static RequestSpecification requestSpec;
+
+    RequestSpecification requestSpecification() {
+        if(requestSpec == null) {
+            PrintStream log = null;
+            try {
+                log = new PrintStream(new FileOutputStream("logging.txt"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            requestSpec = new RequestSpecBuilder()
+                    .setBaseUri(getGlobalValue("baseURL"))
+                    .addQueryParam("key", "qaclick123")
+                    .addFilter(RequestLoggingFilter.logRequestTo(log))
+                    .addFilter(ResponseLoggingFilter.logResponseTo(log))
+                    .setContentType(ContentType.JSON).build();
+
+            return requestSpec;
+        }
+        return requestSpec;
+        }
+
+    private String getGlobalValue(String key) {
+        Properties properties = new Properties();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("C:\\Users\\kwokl\\Sparta Global\\" +
+                    "StarWarsApiTestingFramework\\src\\test\\resources\\global.properties");
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty(key);
+    }
+}
